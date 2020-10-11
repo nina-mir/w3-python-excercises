6048a86f33bd587d25b5a1a3356ccf33fd4e8be2
nina-mir <ninamirf@gmail.com>
2020-10-10 23:57:48 -0700

    Array prob.3 solved

--- array_exercises.py
+++ array_exercises.py
@@ -1,3 +1,8 @@
+from array import *
+
+
+arr = [10, 20, 30, 40, 50]
+
 # 1. Write a Python program to create an array of 5 integers and display the array items. 
 # Access individual element through indexes. Go to the editor
 def prob_1():
@@ -6,8 +11,17 @@ def prob_1():
         arr.append(i*i)
     return arr
 
-arr = prob_1()
+# arr = prob_1()
 
-print(arr[2], arr[4])
+# print(arr[2], arr[4])
+
 # 2. Write a Python program to append a new item to the end of the array. Go to the editor
+
+# 3. Write a Python program to reverse the order of the items in the array. Go to the editor
+
+def prob_3(arr):
+    arr.reverse()    
+prob_3(arr)
+
+print(arr)
 
