bb70cdc4a31fe1a4606dd8f5b6d75c7c9e729cd2
nina-mir <ninamirf@gmail.com>
2020-10-12 19:52:41 -0700

    Array Q3 & Q.4 solved

--- array_exercises.py
+++ array_exercises.py
@@ -2,7 +2,7 @@ from array import *
 
 
 arr = [10, 20, 30, 40, 50]
-
+ar = array('i', [0,1,2,3,4])
 # 1. Write a Python program to create an array of 5 integers and display the array items. 
 # Access individual element through indexes. Go to the editor
 def prob_1():
@@ -15,13 +15,27 @@ def prob_1():
 
 # print(arr[2], arr[4])
 
-# 2. Write a Python program to append a new item to the end of the array. Go to the editor
+# 2. Write a Python program to append a new item to the end of the array.
 
-# 3. Write a Python program to reverse the order of the items in the array. Go to the editor
+# 3. Write a Python program to reverse the order of the items in the array.
 
 def prob_3(arr):
     arr.reverse()    
-prob_3(arr)
+# prob_3(arr)
 
-print(arr)
+# 4. Write a Python program to get the length in bytes of one array item in the internal representation. 
+def prob_4():
+    arr = array('i', [1])
+    print('unsigned int: ' + str(arr.itemsize))
 
+prob_4()
+
+# 5. Write a Python program to get the current memory address and the length 
+# in elements of the buffer used to hold an array's contents and also find the 
+# size of the memory buffer in bytes. Go to the editor
+
+def prob_5(arr):
+    buff = arr.buffer_info()
+    print(buff, buff[1] * arr.itemsize)
+
+prob_5(ar)
