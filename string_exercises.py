a845d75b1d0600e08be837c7d3e2eca7a7e340b7
nina-mir <ninamirf@gmail.com>
2020-11-01 07:51:52 -0800

    Q_18 String solved

--- string_exercises.py
+++ string_exercises.py
@@ -202,7 +202,19 @@ def Q_17(word):
 
 print(Q_17("betrachten"))
 
+# 18. Write a Python function to get a string made of its first three characters of 
+# a specified string. If the length of the string is less than 3 then return the original string. 
+# Sample function and result :
+# first_three('ipy') -> ipy
+# first_three('python') -> pyt
 
+def Q_18(word):
+    if len(word) < 3:
+        return word
+    else:
+        return word[:3]
+
+print ( Q_18('Bau'))
 
 
 
