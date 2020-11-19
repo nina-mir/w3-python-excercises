c1e19ef9d7c97ea26951b3db7ae14d7b36db4d78
nina-mir <ninamirf@gmail.com>
2020-11-19 05:13:27 -0800

    prob.3 solved

--- pandas/DataSeries.py
+++ pandas/DataSeries.py
@@ -20,3 +20,20 @@ print(john)
 # 2. Write a Pandas program to convert a Panda module Series to Python list and it's type. 
 print(john.to_markdown())
 print(john.to_list())
+
+# 3. Write a Pandas program to add, subtract, multiple and divide two Pandas Series.
+# Sample Series: [2, 4, 6, 8, 10], [1, 3, 5, 7, 9]
+
+def prob_3(list_1, list_2, operator):
+    series_1 = pd.Series(list_1, name = 'first')
+    series_2 = pd.Series(list_2, name = 'second')
+    if operator == '+':
+        return series_1 + series_2
+    elif operator == '-':
+        return series_1 - series_2
+    elif operator == '*':
+        return series_1 * series_2
+    else:
+        return series_2 / series_2
+
+print( prob_3([2, 4, 6, 8, 10], [1, 3, 5, 7, 9], '/'))
